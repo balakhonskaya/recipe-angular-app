f29afead9f9406c5cf54544d16b0bc66c29eb66d
balakhonkaya <anthemion@yandex.ru>
2022-03-18 11:39:42 +0100

    Update recipe-detail component and make seection work

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+@Output() recipeWasSeleced = new EventEmitter<Recipe>();
 recipies: Recipe[] =[
-  new Recipe('A Test Recipe','This is test description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'  ),
-  new Recipe('A Test Recipe','This is test description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'  )
+  new Recipe('A Test 1 Recipe','This is test 1 description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'  ),
+  new Recipe('A Test 2 Recipe','This is test 2 description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'  )
 ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSeleced.emit(recipe);
   }
 
 }
